1c617fef0e3833ea0426022e8b132ce16c7e6800
Belen Ortega <belenortega@MacBook-Air-de-Belen-5.local>
2025-11-13 22:11:11 -0300

    Inicializacion y archivos de Astro para GitHub Pages

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    // URL de tu repositorio. Reemplaza 'Maria-josee' y 'nombre-del-repo'
+  site: 'https://Maria-josee.github.io', 
+  // Nombre de tu repositorio en GitHub
+  base: '/reconstruccion-portfolio', 
+  // Otras configuraciones (opcionales)
+  // ...
+});
